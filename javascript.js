c7e3f85e8e078577ac9d964daa1d499fef3e8989
Yong Liang <yongliang24@gmail.com>
2019-07-02 21:04:31 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
 
+const navSupport = document.getElementById("supportNav")
+const mainContent = document.getElementById("mainContent")
+const addCard = document.getElementById("addCard")
+
+const homePage = document.getElementById("homePage")
+const homeDescription = document.createElement("P")
+
+homePage.addEventListener("click", function(){
+  mainContent.innerHTML =""
+  homeDescription.innerText = "Exchange On-Demand allows you to pay foreign currencies on demand with a simple tap"
+  
+  mainContent.appendChild(homeDescription)
+})
+
+ const telSupport = document.createElement("P")
+navSupport.addEventListener("click", function(){
+  mainContent.innerHTML =""
+  telSupport.innerHTML = "Call us at: 555-555-5545"
+  mainContent.appendChild(telSupport)  
+})
+
+   const cardInput = document.createElement("INPUT")
+   cardInput.type="number"
+   cardInput.placeholder ="Card Number"
+  
+    const cardButton = document.createElement("Button")
+    cardButton.type="submit"
+    cardButton.innerText ="Add Card"
+
+  const cardForm = document.createElement("FORM")
+  cardForm.method = "POST"
+  
+  cardForm.addEventListener("click", function(){
+    event.preventDefault()
+  })
+addCard.addEventListener("click", function(){ 
+  mainContent.innerHTML =""
+  cardForm.appendChild(cardInput)
+  cardForm.appendChild(cardButton)
+  mainContent.appendChild(cardForm)
+})
